041ed147377aac260630ec68fd81accb6e7d96ca
Dontecare <Dontecare1@gmail.com>
2024-09-11 16:13:51 -0500

    section 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+//actions
+import { logoutAction } from "./actions/logout";
+
+// routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />
+      }, 
+      {
+        path: "logout",
+        action: logoutAction
+      }
+    ]
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+      <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;

542d2f667768ba104fbdf59c7119ae73e3bc37cf
Dontecare <Dontecare1@gmail.com>
2024-09-11 17:56:04 -0500

    fixed login toast

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -22,9 +22,9 @@ export async function dashboardAction({request}){
   const formData = Object.fromEntries(data)
   try {
     localStorage.setItem("userName", JSON.stringify(formData.userName))
-    return toast.success('Welcome, ${formData.userName}')
+    return toast.success(`Welcome, ${formData.userName}`)
   } catch(e){
-    throw new error("There was a problem creating your account.")
+    throw new Error("There was a problem creating your account.")
   }
 }
 
